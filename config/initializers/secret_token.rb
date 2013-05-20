7f5b83887e235027b98066c0191b76398c0219d2
erikhatcher <erik.hatcher@lucidworks.com>
2013-05-20 14:52:39 -0400

    Base Rails app pre-Blacklight

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LwsBlacklight::Application.config.secret_token = 'ad00b7fe0b71280885720f7c35bbffe5e8020758ea556bce07846ed4719178322f09972c486394f694e71d90d96d7aa9ce7d39b84151f1e588844e8efffc71da'
