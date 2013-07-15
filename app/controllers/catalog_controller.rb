ecda99a29960b3356a2a9c0459a56b9f4f8c4f8c
Yann Yu <yannyu84@gmail.com>
2013-07-15 13:40:37 -0700

    Fix pagination bug related to default row_count
    If you perform a search and try to paginate, it'll throw an error for
    not having a :rows set. This should fix it.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -12,6 +12,7 @@ class CatalogController < ApplicationController
       :'hl.simple.pre' => '<b>',
       :'hl.simple.post' => '</b>',
       :req_type => 'main', # see http://docs.lucidworks.com/display/lweug/Constructing+Solr+Queries
+      :rows => 10
     }
 
     ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SolrHelper#solr_doc_params) or 
