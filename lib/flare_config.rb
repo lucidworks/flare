ede493465d521f2fba7c90db2adb6a516cc4698c
Erik Hatcher <erikhatcher@client-216-30-180-50.dynamic.getopenspace.com>
2013-09-10 15:34:08 -0400

    FOCUS-4886: Fix admin UI URL when inside LWS

--- lib/flare_config.rb
+++ lib/flare_config.rb
@@ -3,7 +3,7 @@ module FlareConfig
   class << self
     def solr_url
 
-      # TODO: modify this to have the LucidWorks-specific environment variables factored brought in under some config/*.rb file so it's an app-level setting
+      # TODO: modify this to have the LucidWorks-specific environment variables brought in under some config/*.rb and config/environments file so it's an app/env-level setting too
 
       @solr_url ||= ENV['SOLR_URL'] ||
           ("#{ENV['LUCIDWORKSCOREADDRESS']}/solr" if ENV['LUCIDWORKSCOREADDRESS']) ||
@@ -24,7 +24,7 @@ module FlareConfig
       # http://localhost:8989/admin/collections/collection1/document?id=file%3A%2FUsers%2Ferikhatcher%2FLWS-2.5.3%2Fdata%2Fsolr%2Fcores%2Fcollection1_0%2Fdata%2Ftlog%2Ftlog.0000000000000000056
 
       @lws_admin_url ||= ENV['LWS_ADMIN_URL'] ||
-          ("#{ENV['LUCIDWORKSCOREADDRESS']}/api" if ENV['LUCIDWORKSCOREADDRESS']) || # TODO: this needs changing based on LWS environment
+          ("#{ENV['LUCIDWORKSUIADDRESS']}/admin" if ENV['LUCIDWORKSUIADDRESS']) ||
           'http://localhost:8989/admin'
 
       @lws_admin_url
