8450e1538baaeb3da81ea78fd482c47967f7c4cf
Adam Johnson <adam.johnson@lucidworks.com>
2013-09-13 13:29:08 -0700

    facet test
    testing new facet helper

--- app/helpers/original_facets.rb
+++ app/helpers/original_facets.rb
@@ -11,4 +11,10 @@ module FacetsHelper
   def facet_configuration_for_field(field)
     Blacklight::Configuration::FacetField.new(:field => field, :label => field.titleize)
   end
+  
+  def render_selected_facet_value(facet_solr_field, item)
+    #Updated class for Bootstrap Blacklight
+    content_tag(:span, render_facet_value(facet_solr_field, item, :suppress_link => true)) +
+     link_to(content_tag(:i, '', :class => "icon-remove") + content_tag(:span, '[remove]', :class => 'hide-text'), remove_facet_params(facet_solr_field, item, params), :class=>"remove")
+  end
 end
